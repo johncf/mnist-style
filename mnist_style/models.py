5ffffeb951ddf598d29570c4747b981078ac8049
johncf <john.ch.fr@gmail.com>
2023-07-11 00:22:24 +0530

    models: conv blocks with groupnorm

--- mnist_style/models.py
+++ mnist_style/models.py
@@ -2,15 +2,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class ConvBlock(nn.Sequential):
+    def __init__(self, in_channels, out_channels, norm_groups=2, **conv2d_args):
+        super().__init__(
+            nn.Conv2d(in_channels, out_channels, bias=False, **conv2d_args),
+            nn.GroupNorm(norm_groups, out_channels),
+            nn.ReLU(True),
+        )
+
+
 class Encoder(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
         self._seq = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, stride=2, padding=2),
-            nn.ReLU(),
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            ConvBlock(1, 16, kernel_size=5, stride=2, padding=2, norm_groups=4),
+            ConvBlock(16, 32, kernel_size=3, stride=1, padding=1, norm_groups=8),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            nn.ReLU(),
             nn.Flatten(),
             nn.Linear(32 * 7 * 7, latent_dim)
         )
@@ -23,17 +30,17 @@ class Decoder(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
         self.fc = nn.Linear(latent_dim, 32 * 7 * 7)
-        self.conv1 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 4, kernel_size=3, stride=1, padding=1)
+        self.conv1 = ConvBlock(32, 32, kernel_size=3, stride=1, padding=1, norm_groups=8)
+        self.conv2 = ConvBlock(32, 4, kernel_size=3, stride=1, padding=1, norm_groups=2)
         self.conv3 = nn.Conv2d(4, 1, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         x = self.fc(x)
         x = x.view(-1, 32, 7, 7)
         x = F.interpolate(x, scale_factor=2)
-        x = F.relu(self.conv1(x))
+        x = self.conv1(x)
         x = F.interpolate(x, scale_factor=2)
-        x = F.relu(self.conv2(x))
+        x = self.conv2(x)
         x = self.conv3(x)
         return x
 

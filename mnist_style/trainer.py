fbd912d6d5522c06a0b227618cfd93cb15dbdd76
johncf <john.ch.fr@gmail.com>
2023-07-18 18:30:09 +0530

    make encoder classify digits and extract style vector

--- mnist_style/trainer.py
+++ mnist_style/trainer.py
@@ -15,6 +15,7 @@ from mnist_style.persistence import save_models
 @dataclass(slots=True)
 class TrainMetrics:
     mean_autoenc_loss: float = 0.
+    mean_classif_loss: float = 0.
     mean_gener_loss: float = 0.
     mean_discr_fake_loss: float = 0.
     mean_discr_real_loss: float = 0.
@@ -23,6 +24,7 @@ class TrainMetrics:
 @dataclass(slots=True)
 class TestMetrics:
     mean_autoenc_loss: float = 0.
+    mean_classif_loss: float = 0.
     median_feat_distrib_error: float = 0.
 
 
@@ -50,54 +52,56 @@ LossFunction: TypeAlias = Callable[[Tensor, Tensor], Tensor]
 
 @dataclass(slots=True, kw_only=True)
 class SimpleTrainer:
-    encoder: ModelOptHelper
-    decoder: ModelOptHelper
+    autoencoder: ModelOptHelper
     autoenc_loss_func: LossFunction
+    classif_loss_func: LossFunction
     latent_norm_scale: float = 2.
 
     def train_one_epoch(self, dataloader: DataLoader) -> TrainMetrics:
         cumulative_ae_loss = 0.0
+        cumulative_classif_loss = 0.0
         num_samples = 0
 
-        self.encoder.train()
-        self.decoder.train()
+        self.autoencoder.train()
         for batch, label in dataloader:
             batch_size = batch.shape[0]
-            latent_code = self.encoder(batch)
-            decoded_batch = self.decoder(latent_code)
+            class_logits, extra_feats, decoded_batch = self.autoencoder(batch)
 
             ae_loss = self.autoenc_loss_func(decoded_batch, batch)
-            self.autoencoder_optimize(ae_loss)
-            cumulative_ae_loss += ae_loss.item() * batch_size
+            classif_loss = self.classif_loss_func(class_logits, label)
+            self.autoencoder_optimize(ae_loss + classif_loss)
 
+            cumulative_ae_loss += ae_loss.item() * batch_size
+            cumulative_classif_loss += classif_loss.item() * batch_size
             num_samples += batch_size
 
         return TrainMetrics(
             mean_autoenc_loss=cumulative_ae_loss / num_samples,
+            mean_classif_loss=cumulative_classif_loss / num_samples,
         )
 
     def autoencoder_optimize(self, loss: Tensor) -> None:
-        self.encoder.opt.zero_grad()
-        self.decoder.opt.zero_grad()
+        self.autoencoder.opt.zero_grad()
         loss.backward()
-        self.encoder.opt.step()
-        self.decoder.opt.step()
+        self.autoencoder.opt.step()
 
     @torch.inference_mode()
     def test_one_epoch(self, dataloader: DataLoader) -> TestMetrics:
         cumulative_ae_loss = 0.0
+        cumulative_classif_loss = 0.0
         latent_code_batches = []
         num_samples = 0
 
-        self.encoder.eval()
-        self.decoder.eval()
+        self.autoencoder.eval()
         for batch, label in dataloader:
             batch_size = batch.shape[0]
-            latent_code = self.encoder(batch)
-            decoded_batch = self.decoder(latent_code)
+            class_logits, extra_feats, decoded_batch = self.autoencoder(batch)
             ae_loss = self.autoenc_loss_func(decoded_batch, batch)
+            classif_loss = self.classif_loss_func(class_logits, label)
+
             cumulative_ae_loss += ae_loss.item() * batch_size
-            latent_code_batches.append(latent_code.detach().numpy())
+            cumulative_classif_loss += classif_loss.item() * batch_size
+            latent_code_batches.append(extra_feats.detach().numpy())
             num_samples += batch_size
 
         latent_codes = np.concatenate(latent_code_batches)
@@ -107,13 +111,14 @@ class SimpleTrainer:
         ]
         return TestMetrics(
             mean_autoenc_loss=cumulative_ae_loss / num_samples,
+            mean_classif_loss=cumulative_classif_loss / num_samples,
             median_feat_distrib_error=float(np.median(feat_wise_fit_errors)),
         )
 
     def save_models(self, ckpt_dir) -> None:
         model_dict = {
-            "encoder": self.encoder.model,
-            "decoder": self.decoder.model,
+            "encoder": self.autoencoder.model.encoder,
+            "decoder": self.autoencoder.model.decoder,
         }
         save_models(model_dict, ckpt_dir)
 
@@ -125,20 +130,19 @@ class AdversarialTrainer(SimpleTrainer):
 
     def train_one_epoch(self, dataloader: DataLoader, gen_loss_factor=0.1) -> TrainMetrics:
         cumulative_ae_loss = 0.0
+        cumulative_classif_loss = 0.0
         cumulative_gen_loss = 0.0
         cumulative_dis_fake_loss = 0.0
         cumulative_dis_real_loss = 0.0
         num_samples = 0
 
-        self.encoder.train()
-        self.decoder.train()
+        self.autoencoder.train()
         self.discriminator.train()
         for batch, label in dataloader:
             batch_size = batch.shape[0]
-            latent_code = self.encoder(batch)
-            decoded_batch = self.decoder(latent_code)
+            class_logits, style_code, decoded_batch = self.autoencoder(batch)
 
-            fake_input = latent_code.detach()  # detach from generator's graph
+            fake_input = style_code.detach()  # detach from generator's graph
             fake_output = self.discriminator(fake_input)
             dis_fake_loss = self.advers_loss_func(fake_output, torch.ones_like(fake_output))
             real_input = torch.randn_like(fake_input) * self.latent_norm_scale
@@ -147,19 +151,21 @@ class AdversarialTrainer(SimpleTrainer):
             self.discriminator_optimize(dis_fake_loss + dis_real_loss)
 
             ae_loss = self.autoenc_loss_func(decoded_batch, batch)
-            fake_output = self.discriminator(latent_code)  # recompute without detaching
+            classif_loss = self.classif_loss_func(class_logits, label)
+            fake_output = self.discriminator(style_code)  # recompute without detaching
             gen_loss = self.advers_loss_func(fake_output, torch.zeros_like(fake_output))
-            self.autoencoder_optimize(ae_loss + gen_loss * gen_loss_factor)
+            self.autoencoder_optimize(ae_loss + classif_loss + gen_loss * gen_loss_factor)
 
             cumulative_dis_fake_loss += dis_fake_loss.item() * batch_size
             cumulative_dis_real_loss += dis_real_loss.item() * batch_size
             cumulative_ae_loss += ae_loss.item() * batch_size
+            cumulative_classif_loss += classif_loss.item() * batch_size
             cumulative_gen_loss += gen_loss.item() * batch_size
-
             num_samples += batch_size
 
         return TrainMetrics(
             mean_autoenc_loss=cumulative_ae_loss / num_samples,
+            mean_classif_loss=cumulative_classif_loss / num_samples,
             mean_gener_loss=cumulative_gen_loss / num_samples,
             mean_discr_fake_loss=cumulative_dis_fake_loss / num_samples,
             mean_discr_real_loss=cumulative_dis_real_loss / num_samples,
@@ -172,8 +178,8 @@ class AdversarialTrainer(SimpleTrainer):
 
     def save_models(self, ckpt_dir) -> None:
         model_dict = {
-            "encoder": self.encoder.model,
-            "decoder": self.decoder.model,
+            "encoder": self.autoencoder.model.encoder,
+            "decoder": self.autoencoder.model.decoder,
             "discriminator": self.discriminator.model,
         }
         save_models(model_dict, ckpt_dir)
